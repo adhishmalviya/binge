5ea2eedba61e9143dc6b0327e4af724c8ec85237
AdhishMalviya <ajsfn@kjgjg.com>
2020-04-02 19:15:03 +0530

    config files added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express= require('express');
 const Joi=require('joi');
 const genres= require('./routes/genres');
 const home=require('./routes/home');
-
+const config=require('config');
 var app=express();
 app.use(express.json());
 app.use('/genres',genres);
@@ -10,6 +10,10 @@ app.use('/',home);
 app.set('view engine','pug');
 //app.set ('views','./views');//default & optional to set path of views 
 
+
+console.log("Application Name:",config.get('name'));
+console.log("Mail Server",config.get('mail.host'));//export NODE_ENV=development
+console.log("Password",config.get('password'));//export app_password=1234
 
 
 
